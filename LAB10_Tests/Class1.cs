8197f279475d8988de47a0c9b233684a085a761f
LipiZed <kometa1810@gmail.com>
2024-04-05 15:26:53 +0300

    Сделал тесты для того, что работает

--- LAB10_Tests/Class1.cs
+++ LAB10_Tests/Class1.cs
@@ -1,12 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using System;
+using МООПЛР10;
+using System.Windows.Forms;
 
 namespace LAB10_Tests
 {
-    public class Class1
+    [TestFixture]
+    public class Tests
     {
+        private Subject subject;
+        private Label timeLabel;
+        private Label stateLabel;
+        private TimeObserver timeObserver;
+        private StateObserver stateObserver;
+
+        [SetUp]
+        public void Setup()
+        {
+            subject = new Subject();
+            timeLabel = new Label();
+            stateLabel = new Label();
+            timeObserver = new TimeObserver(timeLabel);
+            stateObserver = new StateObserver(stateLabel);
+        }
+
+        [Test]
+        public void TestAttachTimeObserver()
+        {
+            subject.Attach(timeObserver);
+            subject.Activate();
+            Assert.That(timeLabel.Text, Does.StartWith("Activation Time: "));
+        }
+
+        [Test]
+        public void TestDetachTimeObserver()
+        {
+            subject.Attach(timeObserver);
+            subject.Activate();
+            subject.Detach(timeObserver);
+            subject.Activate();
+            Assert.That(timeLabel.Text, Is.Empty);
+        }
+
+        [Test]
+        public void TestAttachStateObserver()
+        {
+            subject.Attach(stateObserver);
+            subject.Activate();
+            Assert.That(stateLabel.Text, Does.StartWith("Current State: Active"));
+        }
+
+        [Test]
+        public void TestDetachStateObserver()
+        {
+            subject.Attach(stateObserver);
+            subject.Activate();
+            subject.Detach(stateObserver);
+            subject.State = false; // Deactivate the subject
+            Assert.That(stateLabel.Text, Is.Empty);
+        }
+
+        [Test]
+        public void TestActivate()
+        {
+            subject.Attach(timeObserver);
+            subject.Attach(stateObserver);
+            subject.Activate();
+            Assert.That(subject.State, Is.True);
+            Assert.That(timeLabel.Text, Does.StartWith("Activation Time: "));
+            Assert.That(stateLabel.Text, Does.StartWith("Current State: Active"));
+            Assert.That(subject.ActivationTime, Is.EqualTo(DateTime.Now).Within(1).Seconds);
+        }
     }
+
 }
